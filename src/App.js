7ee12d2e38341b2c744f0d652c6c602b9586c0a1
JunyaFukano <junyafukano@JunyaFukanonoMacBook-Pro.local>
2019-10-27 07:59:29 +0900

    Add Navbar and Title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { createGlobalStyle } from 'styled-components'
+import { Navbar } from "./Navbar/Navbar";
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin:0;
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,h2,h3{
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
-    <div>HELLO SliceLine</div>
+    <>
+    <GlobalStyle/>
+    <Navbar/>
+    HelloSliceline
+  </>
   );
 }
 
